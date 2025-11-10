1eb89674f413a1dfdb2c020c6b983a6c836e8ca7
Venkateswara Rao <venki_bobby1994@yahoo.com>
2025-11-10 15:55:21 +0530

    Update OpenApiConfig.java

--- src/main/java/io/github/mvrao94/kubeguard/config/OpenApiConfig.java
+++ src/main/java/io/github/mvrao94/kubeguard/config/OpenApiConfig.java
@@ -5,44 +5,43 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${spring.application.name}")
-    private String applicationName;
+  @Value("${spring.application.name}")
+  private String applicationName;
 
-    @Value("${project.version:0.0.1}")
-    private String version;
+  @Value("${project.version:0.0.1}")
+  private String version;
 
-    @Value("${project.description:Kubernetes Security Scanner}")
-    private String description;
+  @Value("${project.description:Kubernetes Security Scanner}")
+  private String description;
 
-    @Bean
-    public OpenAPI kubeguardOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title(applicationName + " API")
-                        .description(description)
-                        .version(version)
-                        .contact(new Contact()
-                                .name("KubeGuard Team")
-                                .url("https://github.com/mvrao94/KubeGuard")
-                                .email("venkateswararaom07@gmail.com"))
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://github.com/mvrao94/KubeGuard/blob/main/LICENSE")))
-                .servers(List.of(
-                        new Server()
-                                .url("http://localhost:8080")
-                                .description("Local Development"),
-                        new Server()
-                                .url("https://api.kubeguard.io")
-                                .description("Production")));
-    }
+  @Bean
+  public OpenAPI kubeguardOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title(applicationName + " API")
+                .description(description)
+                .version(version)
+                .contact(
+                    new Contact()
+                        .name("KubeGuard Team")
+                        .url("https://github.com/mvrao94/KubeGuard")
+                        .email("venkateswararaom07@gmail.com"))
+                .license(
+                    new License()
+                        .name("MIT License")
+                        .url("https://github.com/mvrao94/KubeGuard/blob/main/LICENSE")))
+        .servers(
+            List.of(
+                new Server().url("http://localhost:8080").description("Local Development"),
+                new Server().url("https://api.kubeguard.io").description("Production")));
+  }
 }
