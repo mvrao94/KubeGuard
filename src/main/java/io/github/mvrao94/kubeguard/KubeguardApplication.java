5b9f42328402b0b81129aece1ea5012025842a0c
Venkateswara Rao <venki_bobby1994@yahoo.com>
2026-05-08 14:05:18 +0530

    chore: Update dependencies and improve native build configuration

--- src/main/java/io/github/mvrao94/kubeguard/KubeguardApplication.java
+++ src/main/java/io/github/mvrao94/kubeguard/KubeguardApplication.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class KubeguardApplication {
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     SpringApplication.run(KubeguardApplication.class, args);
   }
 }
